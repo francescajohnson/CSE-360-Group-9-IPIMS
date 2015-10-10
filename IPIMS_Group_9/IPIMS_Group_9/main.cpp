b751f80724301f3c76f22593080c37fd676d38e7
francescajohnson <fjohnso3@asu.edu>
2015-10-10 12:41:21 -0700

    New UIs

--- IPIMS_Group_9/IPIMS_Group_9/main.cpp
+++ IPIMS_Group_9/IPIMS_Group_9/main.cpp
@@ -2,7 +2,10 @@
 #include "Registration.h"
 #include "UpdateHealthcare.h"
 #include "LoggingIn.h"
-
+#include "PatientSearch.h"
+#include "EditProfile.h"
+#include "EmergencyAlert.h"
+#include "Welcome.h"
 
 using namespace System;
 using namespace System::Windows::Forms;
@@ -10,21 +13,25 @@ using namespace System::Windows::Forms;
 [STAThread]
 int main() {
 
-
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
-
-	IPIMSGroup9::LoggingIn formLoggingIn;
-	Application::Run(%formLoggingIn);
-
-	IPIMSGroup9::ScheduleAppointment formScheduleAppt;
-	Application::Run(%formScheduleAppt);
 
 	IPIMSGroup9::Registration formRegistration;
 	Application::Run(%formRegistration);
 
-	IPIMSGroup9::UpdateHealthcareCondition formUpdateHealthcare;
-	Application::Run(%formUpdateHealthcare);
+	IPIMSGroup9::LoggingIn formLoggingIn;
+	Application::Run(%formLoggingIn);
+
+	IPIMS_Group_9::Welcome formWelcome;
+	Application::Run(%formWelcome);
+
+	IPIMS_Group_9::PatientSearch formPatientSearch;
+	Application::Run(%formPatientSearch);
+
+	IPIMS_Group_9::EditProfile formEditProfile;
+	Application::Run(%formEditProfile);
+
+	IPIMS_Group_9::EmergencyAlert formEmergencyAlert;
+	Application::Run(%formEmergencyAlert);
 
 }
-
